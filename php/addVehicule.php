622826e0c949106a67906218831bfb7556926889
xWoozixx <r.jauneau13@gmail.com>
2023-05-03 09:09:52 +1100

    test0.2

---
+++ php/addVehicule.php
@@ -0,0 +1,26 @@
+<?php
+include "connect.php";
+
+// Récupérer les valeurs du formulaire
+$immatriculation = $_POST["immatriculation"];
+$description = $_POST["description"];
+$date = $_POST["date"];
+
+// Échapper les caractères spéciaux pour éviter les injections SQL
+$immatriculation = mysqli_real_escape_string($conn, $immatriculation);
+$description = mysqli_real_escape_string($conn, $description);
+$date = mysqli_real_escape_string($conn, $date);
+
+// Créer la requête SQL pour insérer les données dans la table des véhicules
+$requete = "INSERT INTO test (IMMAT, DESC, DateEntréeGarage) VALUES ('$immatriculation', '$description', '$date')";
+
+// Exécuter la requête SQL
+if (mysqli_query($conn, $requete)) {
+  echo "Véhicule enregistré avec succès.";
+} else {
+  echo "Erreur: " . mysqli_error($conn);
+}
+
+// Fermer la connexion à la base de données
+mysqli_close($conn);
+?>
