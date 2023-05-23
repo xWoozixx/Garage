4b330573f9d9faee0ccf1b814a5f9ab4099773d1
cl4wd <shunbouye14@gmail.com>
2023-05-23 22:42:28 +1100

    recherche voiture

--- T21/rechercherVoiture.php
+++ T21/rechercherVoiture.php
@@ -10,18 +10,24 @@ if (!isset($_SESSION["login"])) {
 ?>
 <!DOCTYPE html>
 <html>
-<title>Garage 21</title>
-<?php include_once 'include/T_head.php' ?>
-<?php include_once 'include/T_header.php' ?>
+
+<head>
+    <title>Garage 21</title>
+    <?php include_once 'include/T_head.php' ?>
+    <?php include_once 'include/T_header.php' ?>
+</head>
 
 <body id="haut_de_page">
-    <h1 class="titre_page">
-        <div class="titre_important">Garage</div>
-    </h1>
     <section class="contenu">
         <div>
+            <h1 class="titre_page">
+                Garage
+            </h1>
+            <br>
+            <br>
             <?php
             include "../include/connect.php";
+
             // Récupère l'immatriculation saisie dans le formulaire
             $immatriculation = $_SESSION['message'];
 
@@ -38,9 +44,12 @@ if (!isset($_SESSION["login"])) {
             $description = $row['description'];
 
             echo "<br><h1>Véhicule immatriculé <strong>" . $immatriculation . "</strong></h1><br><br>";
+
             mysqli_close($conn);
             ?>
-            <h1>Modifier </h1><br><br>
+            <h1>Modifier le véhicule</h1>
+            <br>
+            <br>
 
             <form method="post" action="php/updateVehicule.php">
                 <input type='hidden' id="immatriculation" name="immatriculation" pattern="[0-9]{6}NC"
@@ -59,34 +68,41 @@ if (!isset($_SESSION["login"])) {
                         echo "<option value=\"$idMarque\" $selected>$nomMarque</option>";
                     }
                     ?>
-                </select><br>
+                </select>
+                <br><br><br>
 
                 <label for="modele">Modèle :</label>
                 <input type="text" id="modele" name="modele" value="<?php echo $modele; ?>"
-                    pattern="^[a-zA-Z][0-9]{1,20}$" placeholder="Ex: Clio" required> Uniquement des lettres et 20
-                max<br>
-
+                    pattern="^[a-zA-Z][0-9]{1,20}$" placeholder="Ex: Clio" required>
+                <small><br>Uniquement des lettres et 20 caractères max</small>
+                <br><br><br>
                 <label for="dateC">Date de première mise en circulation du véhicule :</label>
-                <input type="date" id="dateC" name="dateC" value="<?php echo $dateC; ?>" required><br>
+                <input type="date" id="dateC" name="dateC" value="<?php echo $dateC; ?>" required>
+                <br><br><br>
 
                 <label for="prixVente">Prix de vente :</label>
                 <input type="number" id="prixVente" name="prixVente" value="<?php echo $prixVente; ?>" min="0"
-                    max="9999999999" step="1" placeholder="Ex: 1000000" required>Uniquement un entier max:
-                9999999999<br>
+                    max="9999999999" step="1" placeholder="Ex: 1000000" required>
+                <small><br>Uniquement un entier (max : 9999999999)</small>
+                <br><br><br>
 
                 <label for="dateEntreeGarage">Date d'entrée au garage :</label>
                 <input type="date" id="dateEntreeGarage" name="dateEntreeGarage"
-                    value="<?php echo $dateEntreeGarage; ?>" required><br>
+                    value="<?php echo $dateEntreeGarage; ?>" required>
+                <br><br><br>
 
                 <label for="chevauxF">Chevaux fiscaux :</label>
                 <input type="number" id="chevauxF" name="chevauxF" value="<?php echo $chevauxF; ?>" min="0" max="999"
-                    placeholder="Ex: 5" required><br>
+                    placeholder="Ex: 5" required>
+                <small><br>Uniquement un entier (max : 999)</small>
+                <br><br><br>
 
                 <label for="description">Description :</label>
                 <textarea id="description" name="description"
-                    placeholder="Rapide description du véhicule"><?php echo $description; ?></textarea> Optionnel<br>
+                    placeholder="Rapide description du véhicule"><?php echo $description; ?></textarea>
+                <small><br>Optionnel</small>
+                <br><br><br>
 
-                <br>
                 <input type="submit" value="Appliquer les modifications">
             </form>
             <form method="post" action="php/delete.php">
